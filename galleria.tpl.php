339126c527cf1486dc725c92765fc339f5b9d377
Mark Theunissen <mark.theunissen@gmail.com>
2009-06-08 08:52:17 +0000

    #484022 by worldwidemv: Empty imagefields should not display

--- galleria.tpl.php
+++ galleria.tpl.php
@@ -6,20 +6,23 @@
  * Default output for a galleria node.
  */
 ?>
-<div id="galleria-content">
-  <div id="main-image"></div>
-  <?php print $thumbnails; ?>
-  <script type="text/javascript">
-    <!--//--><![CDATA[//><!--
-    //Hide images while Galleria is loading
-    $('ul.gallery img').css('display', 'none');
-    //--><!]]>
-  </script>
 
-  <?php if ($image_count > 1): ?>
-    <div class="galleria-nav">
-      <a onclick="$.galleria.prev(); return false;" href="#">&laquo; <?php print $prev; ?></a> |
-      <a onclick="$.galleria.next(); return false;" href="#"><?php print $next; ?> &raquo;</a>
-    </div>
-  <?php endif; ?>
-</div>
+<?php if ($image_count > 0): ?>
+  <div id="galleria-content">
+    <div id="main-image"></div>
+    <?php print $thumbnails; ?>
+    <script type="text/javascript">
+      <!--//--><![CDATA[//><!--
+      //Hide images while Galleria is loading
+      $('ul.gallery img').css('display', 'none');
+      //--><!]]>
+    </script>
+
+    <?php if ($image_count > 1): ?>
+      <div class="galleria-nav">
+        <a onclick="$.galleria.prev(); return false;" href="#">&laquo; <?php print $prev; ?></a> |
+        <a onclick="$.galleria.next(); return false;" href="#"><?php print $next; ?> &raquo;</a>
+      </div>
+    <?php endif; ?>
+  </div>
+<?php endif; ?>
