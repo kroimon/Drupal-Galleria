7b70709a5ee6cdd8bb992001dde40d300ebf1f21
Miro Dietiker <miro.dietiker@md-systems.ch>
2011-05-28 14:38:39 +0200

    codestyle: no tabs

--- js/galleria.load.js
+++ js/galleria.load.js
@@ -1,8 +1,7 @@
 (function ($) {
-
   Drupal.behaviors.galleria = {
     attach: function(context, settings) {
-	  jQuery('.galleria-content', context).galleria(Drupal.settings.galleria.opt);
+      jQuery('.galleria-content', context).galleria(Drupal.settings.galleria.opt);
     }
   }
 })(jQuery);
