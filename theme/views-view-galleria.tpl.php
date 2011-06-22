1cc9bd1e9954467a00bebd6ba43fd35a626e47b8
Sebastian Leu <sebastian.leu@md-systems.ch>
2011-06-22 16:31:03 +0200

    #1196186 by s_leu, NicoH: Fatal Error after changing or creating an image style

--- theme/views-view-galleria.tpl.php
+++ theme/views-view-galleria.tpl.php
@@ -9,22 +9,13 @@ print render($script_container);
 ?>
 <div class="galleria-content clearfix">
 <?php 
-  foreach ($rows as $key => $row) {
-    if (property_exists($row->_field_data['nid']['entity'], $galleria_image_source)) {
-      $lang = $row->_field_data['nid']['entity']->language;
-      $img_data = $row->_field_data['nid']['entity']->{$galleria_image_source}[$lang][0];
-      echo '
-      <div class="field-item">
-        <img src="' . file_create_url($img_data['uri']) . '" 
-             title="' . $img_data['title'] . '" 
-             alt="' . $img_data['alt'] . '" 
-        />
-      </div>';
+  if (count($galleria_elements) > 0) {
+    foreach ($galleria_elements as $key => $element) {
+      echo '<div class="field-item">' . print render($element) . '</div>';
     }
-    else {
-      drupal_set_message('Error: The provided data for this views does not contain the selected image field.', 'error');
-      break;
-    }
+  }
+  else {
+    drupal_set_message('ERROR: No images were found, please check your view results.', 'error');
   }
 ?>
 </div>
