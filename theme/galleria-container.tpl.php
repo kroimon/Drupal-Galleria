d7dae417eb291095767c34bf49acd865f5735054
kroimon <drupal.org@kroimon.de>
2011-07-15 03:09:30 +0200

    Re-added Views support.

--- theme/galleria-container.tpl.php
+++ theme/galleria-container.tpl.php
@@ -5,7 +5,5 @@
 */
 ?>
 <div class="galleria-content clearfix" id="galleria-<?php print $id; ?>">
-  <?php foreach ($items as $item): ?>
-    <div class="field-item"><?php print render($item); ?></div>
-  <?php endforeach; ?>
+  <?php print render($items); ?>
 </div>
